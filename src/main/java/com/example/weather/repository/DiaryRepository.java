4ad8ece42b11d5cb7c01a7bf70eb490bd0b1ae0e
cksghks89 <song960705@naver.com>
2022-11-23 12:18:54 +0900

    feat: PUT "update/diary" 기능 구현
    param date의 첫번째 일기 text를 변경

--- src/main/java/com/example/weather/repository/DiaryRepository.java
+++ src/main/java/com/example/weather/repository/DiaryRepository.java
@@ -6,10 +6,10 @@ import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface DiaryRepository extends JpaRepository<Diary, Integer> {
     List<Diary> findAllByDate(LocalDate date);
     List<Diary> findAllByDateBetween(LocalDate startDate, LocalDate endDate);
+    Diary getFirstByDate(LocalDate date);
 }
