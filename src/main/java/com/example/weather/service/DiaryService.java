9831f53861fb91438ab384c2d2f3defff7ea7cbc
cksghks89 <song960705@naver.com>
2022-11-23 14:14:35 +0900

    feat: DELETE "delete/diary" 기능 구현
    param date 의 모든 일기를 삭제

--- src/main/java/com/example/weather/service/DiaryService.java
+++ src/main/java/com/example/weather/service/DiaryService.java
@@ -113,4 +113,8 @@ public class DiaryService {
 
         diaryRepository.save(nowDiary);
     }
+
+    public void deleteDiary(LocalDate date) {
+        diaryRepository.deleteAllByDate(date);
+    }
 }
