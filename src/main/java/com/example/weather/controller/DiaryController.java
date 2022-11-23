f30957f18cb50a80ffa0c34d23a517493efa84c4
cksghks89 <song960705@naver.com>
2022-11-23 09:47:55 +0900

    feat: 일기 저장 REST API 기능 구현
    외부 API를 통해 입력한 날짜의 기상정보 받아오기 구현
    mysql DB에 text와 기상정보 저장

--- src/main/java/com/example/weather/controller/DiaryController.java
+++ src/main/java/com/example/weather/controller/DiaryController.java
@@ -10,5 +10,11 @@ import java.time.LocalDate;
 @RestController
 @RequiredArgsConstructor
 public class DiaryController {
+    private final DiaryService diaryService;
 
+    @PostMapping("/create/diary")
+    void createDiary(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date,
+                     @RequestBody String text) {
+        diaryService.createDiary(date, text);
+    }
 }
--- src/main/java/com/example/weather/domain/Diary.java
+++ src/main/java/com/example/weather/domain/Diary.java
@@ -1,7 +1,37 @@
 package com.example.weather.domain;
 
-import javax.persistence.Entity;
+import com.sun.istack.NotNull;
+import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 public class Diary {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+
+    @NotNull
+    private String weather;
+
+    @NotNull
+    private String icon;
+
+    @NotNull
+    private double temperature;
+
+    @NotNull
+    private String text;
+
+    @NotNull
+    private LocalDate date;
 }
