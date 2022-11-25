815fb62ef5d7391f0a0baf4c874e97bdda259918
cksghks89 <song960705@naver.com>
2022-11-25 20:26:58 +0900

    feat: Global Exception Handler 구현

--- src/main/java/com/example/weather/service/DiaryService.java
+++ src/main/java/com/example/weather/service/DiaryService.java
@@ -3,6 +3,7 @@ package com.example.weather.service;
 import com.example.weather.WeatherApplication;
 import com.example.weather.domain.DateWeather;
 import com.example.weather.domain.Diary;
+import com.example.weather.error.InvalidDate;
 import com.example.weather.repository.DateWeatherRepository;
 import com.example.weather.repository.DiaryRepository;
 import lombok.RequiredArgsConstructor;
@@ -137,7 +138,9 @@ public class DiaryService {
 
     @Transactional(readOnly = true)
     public List<Diary> readDiary(LocalDate date) {
-        logger.debug("read diary");
+        if(date.isAfter(LocalDate.ofYearDay(3050, 1))){
+            throw new InvalidDate();
+        }
         return diaryRepository.findAllByDate(date);
     }
 
